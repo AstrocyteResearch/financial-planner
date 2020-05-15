f4b083e9091e370b7099a750db9214adb392676d
Zehao Dong <62211561+ZehaoDong32@users.noreply.github.com>
2020-05-15 01:09:44 -0400

    Update monthly_income.py

--- financial_planner/entities/monthly_income.py
+++ financial_planner/entities/monthly_income.py
@@ -4,7 +4,15 @@ class MonthlyIncome(object):
     """
 
     def __init__(self, salary=50000, salary_growth_rate=0.01, savings_rate=0.05, date=None):
+        assert salary >= 0
+        assert salary_growth_rate >= 0
+        assert savings_rate >= 0
+        if date != None:
+            assert isinstance(date, int)
+            if date < 0 or date > 28:
+                raise ValueError('date should be in range 0 to 28')
         self.salary = salary
         self.salary_growth_rate = salary_growth_rate
         self.savings_rate = savings_rate
         self.date = date
+
