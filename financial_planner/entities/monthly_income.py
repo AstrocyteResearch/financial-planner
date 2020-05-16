ae7036648ddc7d217618b034120c684102c09aa6
Zehao Dong <62211561+ZehaoDong32@users.noreply.github.com>
2020-05-16 13:52:51 -0400

    Update monthly_income.py

--- financial_planner/entities/monthly_income.py
+++ financial_planner/entities/monthly_income.py
@@ -1,18 +1,20 @@
-class MonthlyIncome(object):
+from financial_planner.entities.income import Job
+
+
+class MonthlyIncome(Job):
     """
        Define income that is received on a specific day in a month
     """
 
-    def __init__(self, salary=50000, salary_growth_rate=0.01, savings_rate=0.05, date=None):
+    def __init__(self, salary=50000, salary_growth_rate=0.01, savings_rate=0.05, date=None, name='income'):
         assert salary >= 0
         assert salary_growth_rate >= 0
         assert savings_rate >= 0
+        assert isinstance(name, str)
         if date != None:
             assert isinstance(date, int)
             if date < 0 or date > 28:
                 raise ValueError('date should be in range 0 to 28')
-        self.salary = salary
-        self.salary_growth_rate = salary_growth_rate
-        self.savings_rate = savings_rate
+        Job.__init__(self, salary, salary_growth_rate, savings_rate)
+        self.name = name
         self.date = date
-
