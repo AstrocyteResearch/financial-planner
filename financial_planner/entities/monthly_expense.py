c41c0f1857e02debb62c37472e4a1d68d7e166dd
Zehao Dong <62211561+ZehaoDong32@users.noreply.github.com>
2020-05-16 13:52:54 -0400

    Update monthly_expense.py

--- financial_planner/entities/monthly_expense.py
+++ financial_planner/entities/monthly_expense.py
@@ -1,15 +1,17 @@
-class MonthlyExpenses(object):
+from financial_planner.entities.expense import Expenses
+
+
+class MonthlyExpenses(Expenses):
     """
-        Define expense that is paid on a specific day in a month, date=None means daily expense
+        Define expense that is paid on a specific day in a month
     """
 
-    def __init__(self, name, amount=0, date=None):
+    def __init__(self, name='expense', amount=0, as_of_date=None, date=None):
         assert isinstance(name, str)
         assert amount >= 0
         if date != None:
             assert isinstance(date, int)
             if date < 0 or date > 28:
                 raise ValueError('date should be in range 0 to 28')
-        self.name = name
-        self.amount = amount
+        Expenses.__init__(self, name, amount, as_of_date)
         self.date = date
